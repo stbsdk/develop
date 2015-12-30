747803ec228a6bddd11757d37e8f350778d8a177
DarkPark <darkpark@pisem.net>
2015-12-30 17:00:08 +0200

    init load sequence

--- index.js
+++ index.js
@@ -4,3 +4,20 @@
  */
 
 'use strict';
+
+// shims
+require('stb-shim-bind');
+require('stb-shim-classlist');
+require('stb-shim-frame');
+
+// inherit SPA tools
+require('spa-develop/static');
+
+// STB tools
+//require('./proxy');
+//require('./weinre');
+
+// the application itself
+// "js" directory is resolved by webpack to
+// path.join(process.env.PATH_ROOT, process.env.PATH_SRC, 'js')
+require('js/main');
